8c6b4104512e116fa76d664c85b3318905ae57c4
VictorRamirez26 <victorriver6@hotmail.com>
2024-09-20 21:56:38 -0300

    Agregadas clases Departamento

---
+++ ConsorcioUNCode-ejb/src/java/com/consorcio/entity/Departamento.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.consorcio.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author victo
+ */
+@Entity
+public class Departamento implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private String nombre;
+    private boolean eliminado;
+    @ManyToOne
+    private Provincia provincia;
+    
+    public Provincia getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(Provincia provincia) {
+        this.provincia = provincia;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Departamento)) {
+            return false;
+        }
+        Departamento other = (Departamento) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.consorcio.entity.Departamento[ id=" + id + " ]";
+    }
+    
+}
---
+++ ConsorcioUNCode-ejb/src/java/com/consorcio/persist/DAODepartamento.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.consorcio.persist;
+
+import com.consorcio.entity.Departamento;
+import java.util.Collection;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.FlushModeType;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author victo
+ */
+@Stateless
+@LocalBean
+public class DAODepartamento {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    @PersistenceContext
+    private EntityManager em;
+
+    public void guardarDepartamento(Departamento departamento) {
+        em.persist(departamento);
+    }
+
+    public void actualizarDepartamento(Departamento departamento) {
+        em.setFlushMode(FlushModeType.COMMIT);
+        em.merge(departamento);
+        em.flush();
+    }
+
+    public Departamento buscarDepartamentoId(String id) throws NoResultException {
+        return em.find(Departamento.class, id);
+    }
+
+    public Departamento buscarDepartamentoPorNombre(String nombre) {
+        TypedQuery<Departamento> query = em.createQuery("SELECT d FROM Departamento d "
+                + "WHERE d.nombre = :nombre AND d.eliminado = FALSE", Departamento.class);
+        query.setParameter("nombre", nombre);
+        List<Departamento> resultados = query.getResultList();
+
+        if (resultados.isEmpty()) {
+            return null;  // Devuelve null si no hay resultados
+        } else {
+            return resultados.get(0);  // Devuelve el primer resultado
+        }
+    }
+
+    public Collection<Departamento> listarDepartamentoActivo() {
+        try {
+            TypedQuery<Departamento> query = em.createQuery("SELECT d FROM Departamento d "
+                    + "WHERE d.eliminado = FALSE", Departamento.class);
+            return query.getResultList(); 
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    public Collection<Departamento> buscarDepartamentoPorProvinciaYNombre(String nombreDpto , String idProvincia) {
+        try {
+            TypedQuery<Departamento> query = em.createQuery("SELECT d FROM Departamento d "
+            + "WHERE d.provincia.id := idProvincia AMD d.nombre := nombreDpto "
+            + "AND d.elimnado = FALSE", Departamento.class);
+            query.setParameter("nombreDpto", nombreDpto);
+            query.setParameter("idProvincia", idProvincia);
+            return query.getResultList();  
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+}
