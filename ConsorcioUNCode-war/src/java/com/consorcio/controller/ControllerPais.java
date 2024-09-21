f11f4ae1b9e4d11bb5beaeb874234db64bf4507f
JoacoR7 <joaquin.ruiz2012@gmail.com>
2024-09-20 21:24:55 -0300

    Arreglos merge 2

--- ConsorcioUNCode-war/src/java/com/consorcio/controller/ControllerPais.java
+++ ConsorcioUNCode-war/src/java/com/consorcio/controller/ControllerPais.java
@@ -11,7 +11,6 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
